460c6e544afb8012de32c6b49aef82be48ce5adf
fahad_laptop <fmohajir@gmail.com>
2020-10-01 11:46:53 +0400

    Added handler.js

---
+++ handler.js
@@ -0,0 +1,5 @@
+const { AggregateMoodHandler } = require('./handlers')
+
+module.exports.getAllAggregateMoods = AggregateMoodHandler.getAllMoods
+
+module.exports.addAggregateMood = AggregateMoodHandler.addMood
